46af9d1004d81a37123c74e6fe0b00968da77287
Denis Volk <denis.volk@gmail.com>
2022-11-04 08:40:51 +0000

    javascript refactor

--- static/item.js
+++ static/item.js
@@ -1,6 +1,8 @@
-const checklist_items = document.querySelectorAll(".checklist_item")
-
 function toggle_checkbox(description, checkbox_li) {
+  /*
+    toggle checkbox, both in the checklist list, and
+    the item description
+  */
   let checkbox_text = checkbox_li.innerText;
   let checkbox_classes = checkbox_li.querySelector("i").classList;
   let is_checked = checkbox_classes.contains("fa-check-square-o");
@@ -12,6 +14,9 @@ function toggle_checkbox(description, checkbox_li) {
     checkbox_classes.remove("fa-square-o");
     checkbox_classes.add("fa-check-square-o");
   }
+  /*
+    go through description lines and toggle the checklist item
+  */
   let in_lines = description.split("\n");
   var out_lines = [];
   var checklist_re = new RegExp(/- \[(.?)\] (.*)/);
@@ -35,9 +40,46 @@ function toggle_checkbox(description, checkbox_li) {
       out_lines.push(line)
     }
   });
-
+  /*
+    return the new item description with the checklist item toggled
+  */
   return out_lines.join("\n");
 }
+
+function typeInTextarea(newText, el = document.activeElement) {
+  /*
+    insert text into textarea at cursor
+  */
+  const start = el.selectionStart
+  const end = el.selectionEnd
+  const text = el.value
+  const before = text.substring(0, start)
+  const after  = text.substring(end, text.length)
+  el.value = (before + newText + after)
+  el.selectionStart = el.selectionEnd = start + newText.length
+  el.focus()
+}
+
+function insert_date() {
+  /*
+    insert datetime into description at cursor.
+    format yyyy-mm-dd MM:SS
+  */
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2, '0');
+  var mm = String(today.getMonth() + 1).padStart(2, '0');
+  var yyyy = today.getFullYear();
+  var hhh = String(today.getHours()).padStart(2, '0');
+  var mmm = String(today.getMinutes()).padStart(2, '0');
+
+  today = yyyy + '-' + mm + '-' + dd + ' ' + hhh + ':' + mmm
+  typeInTextarea(today, document.getElementById('description'))
+}
+
+/*
+  add click event handler to checklist items
+*/
+const checklist_items = document.querySelectorAll(".checklist_item")
 
 checklist_items.forEach(checklist_item => {
   checklist_item.addEventListener('click', (e) => {
@@ -46,3 +88,9 @@ checklist_items.forEach(checklist_item => {
     desc_div.textContent = new_description;
   });
 });
+
+/*
+  scroll description to the bottom
+*/
+var textarea = document.getElementById('description');
+textarea.scrollTop = textarea.scrollHeight;
